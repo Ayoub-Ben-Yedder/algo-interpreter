7de317c4c9bda0eb96013dc429ecf78b86e4d3c4
Ayoub-Ben-Yedder <byedderayoub@gmail.com>
2024-11-20 21:49:47 +0100

    Added More Tokens and updated the Lexer

--- Lexer.java
+++ Lexer.java
@@ -14,7 +14,7 @@ public class Lexer {
     }
     private void readChar(){
         if (readPosition >= input.length()) {
-            currentChar = '\0'; // Null character indicates end of input
+            currentChar = '\0';
         } else {
             currentChar = input.charAt(readPosition);
         }
@@ -24,6 +24,7 @@ public class Lexer {
     public Token nextToken(){
         skipWhitespace();
         Token token;
+        char nextChar = readPosition < input.length() ? input.charAt(readPosition) : '\0';
         switch (currentChar) {
             case '+':
                 token = new Token(TokenType.PLUS, Character.toString(currentChar));
@@ -46,8 +47,109 @@ public class Lexer {
             case '\0':
                 token = new Token(TokenType.EOF, "EOF");
                 break;
+            case '=':
+                token = new Token(TokenType.EQ, Character.toString(currentChar));
+                break;
+            case '<':
+                if(nextChar == '='){
+                    token = new Token(TokenType.LQ, "<=");
+                    readChar();
+                }else if(nextChar == '-'){
+                    token = new Token(TokenType.ASSIGN, "<-");
+                    readChar();
+                }else{
+                    token = new Token(TokenType.LT, Character.toString(currentChar));
+                }
+                break;
+            case '>':
+                if(nextChar != '='){
+                    token = new Token(TokenType.GT, Character.toString(currentChar));
+                }else{
+                    token = new Token(TokenType.GQ, ">=");
+                    readChar();
+                }
+                break;
+            case ';':
+                token = new Token(TokenType.SEMI, Character.toString(currentChar));
+                break;
+            case ':':
+                token = new Token(TokenType.COLON, Character.toString(currentChar));
+                break;
             default:
-                token = new Token(TokenType.NUMBER, readNumber());
+                if(isDigit(currentChar)){
+                    String num =  readNumber();
+                    if(currentChar=='.'){
+                        readChar();
+                        readChar();
+                        String reel = readNumber();
+                        token = new Token(TokenType.NUMBER, num+'.'+reel);
+                    }else{
+                        token = new Token(TokenType.NUMBER, num);
+                    }
+                }else if(isAlpha(currentChar)){
+                    String word = readString();
+                    switch (word) {
+                        case "algorithme":
+                            token = new Token(TokenType.ALGO, word);
+                            break;
+                        case "TDO":
+                            token = new Token(TokenType.TDO, word);
+                            break;
+                        case "debut":
+                            token = new Token(TokenType.DEBUT, word);
+                            break;
+                        case "fin":
+                            token = new Token(TokenType.FIN, word);
+                            break;
+                        case "tantque":
+                            token = new Token(TokenType.TANTQUE, word);
+                            break;
+                        case "vrai":
+                            token = new Token(TokenType.VRAI, word);
+                            break;
+                        case "faux":
+                            token = new Token(TokenType.FAUX, word);
+                            break;
+                        case "si":
+                            token = new Token(TokenType.SI, word);
+                            break;
+                        case "sinon":
+                            token = new Token(TokenType.SINON, word);
+                            break;
+                        case "finsi":
+                            token = new Token(TokenType.FINSI, word);
+                            break;
+                        case "et":
+                            token = new Token(TokenType.ET, word);
+                            break;
+                        case "ou":
+                            token = new Token(TokenType.OU, word);
+                            break;
+                        case "non":
+                            token = new Token(TokenType.NON, word);
+                            break;
+                        case "entier":
+                            token = new Token(TokenType.ENTIER, word);
+                            break;
+                        case "reel":
+                            token = new Token(TokenType.REEL, word);
+                            break;
+                        case "chaine":
+                            token = new Token(TokenType.CHAINE, word);
+                            break;
+                        case "char":
+                            token = new Token(TokenType.CHAR, word);
+                            break;
+                        case "bool":
+                            token = new Token(TokenType.BOOL, word);
+                            break;
+                        default:
+                            token = new Token(TokenType.IDENTIFIER, word);
+                            break;
+                    }
+                }else{
+                    token = new Token(TokenType.ILLEGAL, Character.toString(currentChar));
+                }
                 break;
         }
         readChar();
@@ -66,8 +168,19 @@ public class Lexer {
         readPosition--;
         return input.substring(startPos, position);
     }
+    private String readString(){
+        int startPos = position;
+        while(isAlpha(currentChar)||isDigit(currentChar)){
+            readChar();
+        }
+        readPosition--;
+        return input.substring(startPos, position);
+    }
     private boolean isDigit(char c){
         return (c>='0' && c<='9');
+    }
+    private boolean isAlpha(char c){
+        return ((c>='A' && c<='Z' ) || (c>='a' && c<='z'));
     }
     public Token[] tokenize(){
         Token tokens[] = new Token[maxTokens];
--- Token.java
+++ Token.java
@@ -1,6 +1,16 @@
 enum TokenType {
-    PLUS, MINUS, MUL, DIVIDE, LPAREN, RPAREN, NUMBER, EOF
+    PLUS, MINUS, MUL, DIVIDE, LPAREN, RPAREN, NUMBER,
+    EQ, NEQ, GT, GQ, LT, LQ, SEMI, COLON,
+    NON, OU, ET,
+    EOF, ILLEGAL,
+    IDENTIFIER, ASSIGN,
+    ALGO, DEBUT, FIN, TDO,
+    SI, SINON, FINSI,
+    TANTQUE,
+    VRAI, FAUX,
+    ENTIER, REEL, CHAINE, CHAR, BOOL,
 }
+
 public class Token {
     public TokenType type;
     public String value;
